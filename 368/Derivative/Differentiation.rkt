f3a43ca747a8169be6b50964df93b9744311b1b5
llammers <llammers@ku.edu>
2015-02-18 16:42:48 -0600

    adding lab 4 and more derivatives

--- 368/Derivative/Differentiation.rkt
+++ 368/Derivative/Differentiation.rkt
@@ -16,18 +16,23 @@
                  1
                  0)))
         ((case (car z)
-          ((+) (if (not (pair? (cdr z)))
+          ((+) (if (not (pair? (cdr z))) ; + with no arguments (derivative of 0)
                    0
-                   (cons '+ (map (λ (y) (symderivat var y)) (cdr z)))))
-          ((*) (if (null? (cdr z))
+                   (cons '+ (map (λ (y) (symderivat var y)) (cdr z))))) ;add derivatives of each element in list
+          ((*) (if (null? (cdr z)) ; * with no arguments (derivative of 1)
                    0
-                   (if (null? (cddr z))
+                   (if (null? (cddr z)) ; * with single argument
                        (symderivat var (cadr z))
-                       (list '+ (list '* (cadr z) (symderivat var (cons '* (cddr z)))) (append (cons '* (cddr z)) (list (symderivat var (cadr z))))))))
+                       (list '+ (list '* (cadr z) (symderivat var (cons '* (cddr z)))) 
+                             (append (cons '* (cddr z)) (list (symderivat var (cadr z))))))))
           ((-) (cons '- (map (λ (y) (symderivat var y)) (cdr z))))
-          ((/) '/)
-          ((exp) 'exp)
-          ((log) 'log)
+          ((/) (if (null? (cddr z)) ; / with single argument
+                   (symderivat var (cons '/ (cons 1 (cdr z))))
+                   (list '/ (list '- (append (cons '* (cddr z)) (list (symderivat var (cadr z))))
+                                  (list '* (cadr z) (symderivat var (cons '* (cddr z)))))
+                         (list 'expt (cons '* (cddr z)) 2))))
+          ((exp) (list '* (symderivat var (cadr z)) z))
+          ((log) (list '* (list '/ 1 (cadr z)) (symderivat var (cadr z)))); derivative cdr z * 1/cdr z
           ((expt) 'expt)
           ((sin) `(* (cos ,(cadr z)) ,(symderivat var (cadr z))))
           ((cos) `(- (* (sin ,(cadr z)) ,(symderivat var (cadr z)))))
@@ -38,4 +43,5 @@
           ((abs) 'abs)
           ))))
 
-(symderivat 'x '(cos (* x x)))
+(symderivat 'x '(log (* 2 x)))
+(*)
