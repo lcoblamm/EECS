e3b972aac661d9ab0f43438659805e5e6214a7f4
llammers <llammers@ku.edu>
2015-04-27 10:25:59 -0500

    Starting on min spanning tree lab

---
+++ 560/lab11/DisjointSet.h
@@ -0,0 +1,26 @@
+/*
+File: DisjointSet.h
+Author: Lynne Coblammers
+Date: 2014.04.27
+*/
+
+#ifndef DISJOINTSET_H
+#define DISJOINTSET_H
+
+#include "DisjointSetNode.h"
+
+class DisjointSet
+{
+public:
+    DisjointSet(int* elements, int numElements);
+    ~DisjointSet();
+
+    int find(int element);
+    void union(int first, int second);
+
+private:
+    DisjointSetNode** m_elements;
+    int m_numElements;
+};
+
+#endif
---
+++ 560/lab11/Queue.h
@@ -0,0 +1,30 @@
+/*
+  File: Queue.h
+  Author: Lynne Coblammers
+  Date: 2015.03.30
+*/
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include "QueueNode.h"
+
+template <typename T>
+class Queue
+{
+public:
+  Queue();
+  ~Queue();
+  
+  void enqueue(T item);
+  T dequeue();
+  bool isEmpty();
+  T peek();
+  
+private:
+  QueueNode<T>* m_head;
+  QueueNode<T>* m_tail;
+  int m_size;
+};
+
+#endif
