d893b7fde5300e7340011d3f66da052a1044daef
llammers <llammers@ku.edu>
2015-10-26 14:29:28 -0500

    Finishing up number guessing game

--- 776/hw03/numberGuess.hs
+++ 776/hw03/numberGuess.hs
@@ -1,18 +1,26 @@
+-- Lynne Coblammers
+-- EECS 776
+-- HW 3
+-- 2015.10.26
+
+import Control.Applicative
+
 main :: IO ()
 main = prompt
+
 
 prompt :: IO ()
 prompt = do
             putStrLn "Guess a number."
             x <- getLine
-            loop (read x)
+            checkGuess (read x)
 
-loop :: Int -> IO ()
-loop i = if i == 4
-          then putStrLn "Correct!"
-          else do
-                if i < 4
-                 then putStrLn "Too low. Guess again."
-                 else putStrLn "Too high. Guess again."
-                x <- getLine
-                loop (read x)
+checkGuess :: Int -> IO ()
+checkGuess i = if i == 4
+                then putStrLn "You win a cookie!"
+                else do
+                  if i < 4
+                    then putStrLn "Too low!"
+                    else putStrLn "Too high!"
+                  prompt
+                
