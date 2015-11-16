b2adc50dd0837b1872f6732401e29b1959fa8eb4
llammers <llammers@ku.edu>
2015-11-16 13:14:49 -0600

    Adding more test files

---
+++ 665/project2/testDoWhile.c
@@ -0,0 +1,8 @@
+double m[6];
+
+scale(double x, int y) 
+{
+    do
+        x = x + 1;
+    while (x < y && x != 10);
+}
--- 665/project2/testlineno.c
+++
@@ -1,8 +0,0 @@
-double m[6];
-
-scale(double x, int y) 
-{
-    if (x == 0 || x == 1) {
-        x = 0;
-    }
-}
