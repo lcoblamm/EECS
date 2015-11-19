33835cfb911bbb03ceca938387f3664ec85fdc1b
llammers <llammers@ku.edu>
2015-11-19 14:14:57 -0600

    Removing unneeded function

--- 665/project2/sem.h
+++ 665/project2/sem.h
@@ -35,4 +35,3 @@ struct sem_rec *rel(char *, struct sem_rec *, struct sem_rec *);
 struct sem_rec *set(char *, struct sem_rec *, struct sem_rec *);
 void startloopscope();
 struct sem_rec *string(char *);
-struct labelTracker *newLabel(char *, struct sem_rec*, int, struct labelTracker*);
