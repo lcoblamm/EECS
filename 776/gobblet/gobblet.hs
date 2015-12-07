58c7dea7c028612b30c5d5cd47b94c8f1432ac25
llammers <llammers@ku.edu>
2015-12-07 14:43:13 -0600

    Adding tic tac toe example and cleaning up data types in gobblet

--- 776/gobblet/gobblet.hs
+++ 776/gobblet/gobblet.hs
@@ -1,28 +1,35 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, GADTs, KindSignatures #-}
+
 module Main where
 
 import Graphics.Blank
 import Data.Text (Text)
 
-data Piece = Biggest | Big | Medium | Small deriving (Eq, Ord)
+data Size = Tiny | Small | Medium | Big deriving (Eq, Ord)
 
-data Turn = White | Black  deriving (Eq, Ord)
+data Color = White | Black  deriving (Eq, Ord)
 
-data Stack :: * where
-  Empty :: Stack
-  Push :: Piece -> Stack -> Stack
+data Piece :: * where
+  Piece :: Size -> Color -> Piece
 
-swap :: Turn -> Turn
+type Stack = [Piece]
+
+swap :: Color -> Color
 swap White = Black
 swap Black = White
+
+loop :: DeviceContext -> Map (Int, Int) Stack -> Color -> IO ()
+loop context board color = do
+  -- print out board
+
 
 whiteColor, blackColor, lineColor :: Text
 whiteColor = "#ffffff"
 blackColor = "#000000"
 lineColor = "#0000ff"
 
-drawBiggest :: Turn -> Double -> Canvas ()
-drawBiggest color maxRad = do 
+drawBig :: Color -> Double -> Canvas ()
+drawBig color maxRad = do 
   beginPath()
   arc(0, 0, maxRad, 0, 2 * pi, False)
   lineWidth 2
@@ -35,24 +42,10 @@ drawBiggest color maxRad = do
   stroke()
   fill()
 
-drawBig :: Turn -> Double -> Canvas ()
-drawBig color maxRad = do 
-  beginPath()
-  arc(0, 0, (maxRad `div` 4) * 3, 0, 2 * pi, False)
-  lineWidth 2
-  case color of
-    White -> strokeStyle blackColor
-    Black -> strokeStyle blackColor
-  case color of
-    White -> fillStyle whiteColor
-    Black -> fillStyle blackColor
-  stroke()
-  fill()
-
-drawMedium :: Turn -> Double -> Canvas ()
+drawMedium :: Color -> Double -> Canvas ()
 drawMedium color maxRad = do 
   beginPath()
-  arc(0, 0, maxRad `div` 2, 0, 2 * pi, False)
+  arc(0, 0, (maxRad * 0.75), 0, 2 * pi, False)
   lineWidth 2
   case color of
     White -> strokeStyle blackColor
@@ -63,10 +56,24 @@ drawMedium color maxRad = do
   stroke()
   fill()
 
-drawSmall :: Turn -> Double -> Canvas ()
+drawSmall :: Color -> Double -> Canvas ()
 drawSmall color maxRad = do 
   beginPath()
-  arc(0, 0, maxRad `div` 4, 0, 2 * pi, False)
+  arc(0, 0, maxRad * 0.5, 0, 2 * pi, False)
+  lineWidth 2
+  case color of
+    White -> strokeStyle blackColor
+    Black -> strokeStyle blackColor
+  case color of
+    White -> fillStyle whiteColor
+    Black -> fillStyle blackColor
+  stroke()
+  fill()
+
+drawTiny :: Color -> Double -> Canvas ()
+drawTiny color maxRad = do 
+  beginPath()
+  arc(0, 0, maxRad * 0.25, 0, 2 * pi, False)
   lineWidth 2
   case color of
     White -> strokeStyle blackColor
