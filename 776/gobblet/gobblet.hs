d90fc825e5f2f96413393d0d1df9716a4a106043
lynnel <lynne.lammers@gmail.com>
2015-12-08 02:27:10 -0600

    Working on being able to move pieces

--- 776/gobblet/gobblet.hs
+++ 776/gobblet/gobblet.hs
@@ -37,23 +37,20 @@ createMap = Map.fromList [((0,1), fullStack Black)
 
 loop :: DeviceContext -> Map (Int, Int) Stack -> Color -> IO ()
 loop context board turn = do
+  let size = min (width context) (height context)
   send context $ do
-    clearRect(0, 0, width context, height context)
-    let size = min (width context) (height context)
+    clearCanvas
     save()
     translate(width context / 2, height context / 2)
-    -- draw vertical lines
-    drawLine (-0.167 * size, -0.33 * size) (-0.167 * size, 0.33 * size)
-    drawLine (0, -0.33 * size) (0, 0.33 * size)
-    drawLine (0.167 * size, -0.33 * size) (0.167 * size, 0.33 * size)
-    -- draw horizontal lines
-    drawLine (-0.33 * size, -0.167 * size) (0.33 * size, -0.167 * size)
-    drawLine (-0.33 * size, 0) (0.33 * size, 0)
-    drawLine (-0.33 * size, 0.167 * size) (0.33 * size, 0.167 * size)
+    -- draw board lines
+    sequence_ [ do 
+      drawLine (x * size, -0.33 * size) (x * size, 0.33 * size)
+      drawLine (-0.33 * size, x * size) (0.33 * size, x * size)
+      | x <- [-0.167, 0, 0.167] ]
     restore()
-    -- print pieces from map
     save()
     translate(width context / 2 - size / 2, height context / 2 - size / 2)
+    -- print pieces from map
     sequence_ [ do 
       case Map.lookup (x,y) board of 
         Just (p:ps) -> drawPiece p ((fromIntegral x) * size / 6.0 + (size * 0.083), (fromIntegral y) * size / 6.0 + (size * 0.083)) (size * 0.075)
@@ -61,13 +58,44 @@ loop context board turn = do
         Nothing -> return () 
       | x <- [0,1,2,3,4,5], y <- [1,2,3,4] ]
     restore()
-  loop context board turn
+    let text = if turn == White then "White's turn" else "Black's turn"
+    printText text (width context / 2, 30)
+
+  event <- wait context
+  case ePageXY event of
+    Nothing -> loop context board turn
+    Just (x,y) -> case getBox (x,y) (width context) (height context) size of
+      Nothing -> loop context board turn
+      Just pos -> case Map.lookup pos board of
+        Nothing -> loop context board turn
+        Just [] -> loop context board turn
+        Just (p:_) -> if (color p /= turn)
+          then loop context board turn
+          else do 
+            event <- wait context
+            case ePageXY event of
+              Nothing -> loop context board turn
+              Just (a,b) -> case getBox (a,b) (width context) (height context) size of
+                Nothing -> loop context board turn
+                Just pos' -> case Map.lookup pos' board of
+                  Nothing -> loop context (Map.insert pos' [p] board) (swap turn)
+                  -- TODO: check that you can put piece here
+                  Just qs -> loop context (Map.insert pos' (p:qs) board) (swap turn)
 
 
-whiteColor, blackColor, lineColor :: Text
+whiteColor, blackColor, lineColor, textColor :: Text
 whiteColor = "#faebd7"
 blackColor = "#000000"
 lineColor = "#0000ff"
+textColor = "#458B00"
+
+printText :: Text -> (Double, Double) -> Canvas ()
+printText text (x,y) = do
+  clearRect (0,0, x * 2,30)
+  font "20pt Calibri"
+  textAlign "center"
+  fillStyle textColor
+  fillText(text, x, y)
 
 drawPiece :: Piece -> (Double, Double) -> Double -> Canvas ()
 drawPiece piece (x,y) maxRad = do 
@@ -101,3 +129,13 @@ drawLine (a, b) (c, d) = do
   strokeStyle lineColor
   stroke()
 
+getBox :: (Double, Double) -> Double -> Double -> Double -> Maybe (Int, Int)
+getBox (x,y) width height size = do
+  let adjX = x - width / 2
+  let adjY = y - height / 2
+  if (((abs adjX) >  size / 2) || ((abs adjY) > size / 2))
+    then Nothing
+    else do
+      let coordX = ceiling $ adjX / (size / 6)
+      let coordY = ceiling $ adjY / (size / 6)
+      Just (coordX, coordY)
