9d4b8852ae00360773e0e622fcdd46bab177e743
lcoblamm <lynne.coblammers@gmail.com>
2016-04-05 22:11:45 -0500

    Adjusting matlab script to account for new csv format

--- 399/dcacheToPlot.m
+++ 399/dcacheToPlot.m
@@ -1,31 +1,18 @@
-filepath = '~/dev/EECS/399/dcachecsv.dat';
+filepath = '~/dev/EECS/399/dcachecsv.out';
 origData = csvread(filepath);
 
-% TODO: decide if this is valid cutoff
+% pull top row off for instruction intervals
+xVals = origData(1,1:end - 1)';
+
+% pull first column off for addresses
+yVals = origData(2:end,1);
+
+% trim matrix data
+heatMatrix = origData(2:end,2:end);
+
 % determine cutoff point for number of accesses
-z = origData(:,3);
-maxAccesses = max(z);
+maxAccesses = max(heatMatrix(:));
 cutoff = 0.05 * maxAccesses;
-
-% find unique values for instruction count and addresses
-x = origData(:,1);
-y = origData(:,2);
-xVals = unique(x);
-yVals = unique(y);
-
-mappedData = origData;
-% replace x values in data with corresponding index in xVals
-for n = 1:size(xVals,1)
-   mappedData(mappedData(:,1) == xVals(n),1) = n; 
-end
-
-% replace y values in data with corresponding index in yVals
-for n = 1:size(yVals,1)
-   mappedData(mappedData(:,2) == yVals(n),2) = n; 
-end
-
-% create matrix containing only # of accesses
-heatMatrix = accumarray(mappedData(:,[2 1]), mappedData(:,3));
 
 % remove rows where none of accesses meet or exceed cutoff
 iRemove = all((heatMatrix(:,:) < cutoff),2);
