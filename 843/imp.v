8d95ef0fcaedb05961bf3665953c864f02f543ba
lcoblamm <lynne.coblammers@gmail.com>
2016-04-07 18:42:49 -0500

    Adding hoare triples and repeat

--- 843/imp.v
+++ 843/imp.v
@@ -25,7 +25,7 @@ Definition empty_state : state :=
   fun _ => 0.
 
 Definition update (st : state) (i : id) (n : nat) : state :=
-  fun x => if (eq_id_dec x i) then n else (st i).
+  fun x => if (eq_id_dec x i) then n else (st x).
 
 Inductive aexp : Type :=
 | ANum : nat -> aexp
@@ -49,7 +49,8 @@ Inductive com : Type :=
 | CSeq : com -> com -> com
 | CIf : bexp -> com -> com -> com
 | CWhile : bexp -> com -> com
-| CFor : com -> bexp -> com -> com -> com.
+| CFor : com -> bexp -> com -> com -> com
+| CRepeat : com -> bexp -> com.
 
 Notation "'SKIP'" :=
   CSkip.
@@ -65,6 +66,8 @@ Notation "'IFB' c1 'THEN' c2 'ELSE' c3 'FI'" :=
   (CIf c1 c2 c3) (at level 80, right associativity).
 Notation "'FOR' c1 b c2 'DO' c3 'END'" :=
   (CFor c1 b c2 c3) (at level 90, right associativity).
+Notation "'REPEAT' e1 'UNTIL' b2 'END'" := 
+  (CRepeat e1 b2) (at level 80, right associativity).
 
 Fixpoint aeval (st : state) (a : aexp) : nat :=
   match a with
@@ -147,7 +150,15 @@ Inductive ceval : com -> state -> state -> Prop :=
                c1 / st || st' ->
                (WHILE b DO (c3 ;; c2) END) / st' || st'' ->
                (CFor c1 b c2 c3) / st || st''
-
+| E_RepeatEnd : forall st st' b c,
+                  c / st || st' ->
+                  beval st' b = true ->
+                  (REPEAT c UNTIL b END) / st || st'
+| E_RepeatLoop : forall st st' st'' b c,
+                   c / st || st' ->
+                   beval st' b = false ->
+                   (REPEAT c UNTIL b END) / st' || st'' ->
+                   (REPEAT c UNTIL b END) / st || st''
   where "c1 '/' st '||' st'" := (ceval c1 st st').
 
 Theorem for_while_equiv : forall b c1 c2 c3 st1 st2,
@@ -159,3 +170,93 @@ Proof.
   [ apply E_Seq with (st' := st')
   | apply E_For with (st' := st')]; assumption; assumption.
 Qed.
+
+Definition Assertion := state -> Prop.
+
+Definition assert_implies (P Q : Assertion) : Prop :=
+  forall st, P st -> Q st.
+
+Definition hoare_triple (P:Assertion) (c:com) (Q:Assertion) : Prop :=
+  forall st st', (c / st || st') -> P st -> Q st'.
+
+Notation "{{ P }} c {{ Q }}" :=
+  (hoare_triple P c Q) (at level 90, c at next level).
+
+Definition ex1_repeat :=
+  REPEAT
+    X ::= ANum 1;;
+    Y ::= APlus (AId Y) (ANum 1)
+    UNTIL (BEq (AId X) (ANum 1)) END.
+
+Theorem ex1_repeat_works :
+  ex1_repeat / empty_state || update (update empty_state X 1) Y 1.
+Proof.
+   unfold ex1_repeat.
+   eapply E_RepeatEnd.
+   eapply E_Seq. apply E_Ass with (n := 1). reflexivity.
+   apply E_Ass. reflexivity.
+   reflexivity.
+Qed.
+
+Definition assn_sub X a P : Assertion :=
+  fun (st : state) => P (update st X (aeval st a)).
+
+Notation "P [ X |-> a ]" := (assn_sub X a P) (at level 10).
+
+Theorem hoare_skip : forall P, {{P}} SKIP {{P}}.
+Proof.
+  intros P.
+  unfold hoare_triple. intros st st' H HP.
+  inversion H. subst. assumption.
+Qed.
+
+Theorem hoare_asgn : forall Q X a, {{Q [X |-> a]}} (X ::= a) {{Q}}.
+Proof.
+  intros Q x a.
+  unfold hoare_triple. intros st st' H HQ.
+  inversion H. subst.
+  unfold assn_sub in HQ. assumption.
+Qed.
+
+Theorem hoare_seq : forall P Q R c1 c2,
+                      {{Q}} c2 {{R}} ->
+                      {{P}} c1 {{Q}} ->
+                      {{P}} (c1 ;; c2) {{R}}.
+Proof.
+  intros P Q R c1 c2.
+  unfold hoare_triple. intros H1 H2 st st' Hs HP.
+  inversion Hs. subst.
+  apply H1 in H6. assumption.
+  apply H2 in H3. assumption.
+  assumption.
+Qed.
+
+Definition bassn b : Assertion :=
+  fun st => (beval st b = true).
+
+Lemma bexp_eval_true : forall b st,
+                         beval st b = true -> (bassn b) st.
+Proof.
+  intros b st H.
+  unfold bassn. assumption.
+Qed.
+
+Lemma bexp_eval_false : forall b st,
+                          beval st b = false -> not ((bassn b) st).
+Proof.
+  intros b st H.
+  unfold not.
+  intros Hb.
+  unfold bassn in Hb.
+  rewrite H in Hb.
+  inversion Hb.
+Qed.
+
+Theorem hoare_if : forall P Q c1 c2 b,
+                     {{fun st => ((P st) and (bassn b st))}} c1 {{Q}} ->
+                     {{fun st => ((P st) and (not (bassn b st)))}} c2 {{Q}} ->
+                     {{P}} (IFB b THEN c1 ELSE c2 FI) {{Q}}.
+
+Lemma hoare_while : forall P b c,
+  {{fun st =>  P st and bassn b st}} c {{P}} ->
+  {{P}} WHILE b DO c END {{fun st => P st and (not (bassn b st))}}.
